36ccfe0f8edb7a5cc127ef5f2f14940bbf6cfc39
ADRIAN EKA SAPUTRA <78060253+adrianeka@users.noreply.github.com>
2026-06-09 16:58:45 +0700

    refactor: update GlobalConfiguration gconfId type and remove unused file

--- src/main/java/com/tujuhsembilan/glucoseclamp/model/GlobalConfiguration.java
+++ src/main/java/com/tujuhsembilan/glucoseclamp/model/GlobalConfiguration.java
@@ -3,14 +3,16 @@ package com.tujuhsembilan.glucoseclamp.model;
 import com.tujuhsembilan.glucoseclamp.model.base.BaseEntity;
 import lombok.*;
 import jakarta.persistence.*;
-import java.time.LocalDateTime;
-import java.util.List;
+
+import java.math.BigInteger;
 
 @Entity
 @Table(name = "global_configurations")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@EqualsAndHashCode(exclude = "gconfId", callSuper = true)
+@ToString(exclude = "gconfId")
 @Builder
 public class GlobalConfiguration extends BaseEntity {
     
@@ -18,14 +20,12 @@ public class GlobalConfiguration extends BaseEntity {
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "global_config_id_seq")
     @SequenceGenerator(name = "global_config_id_seq", sequenceName = "global_config_id_seq", allocationSize = 1)
     @Column(name = "gconf_id")
-    private Integer gconfId;
+    private BigInteger gconfId;
     
     @Column(name = "gconf_code", nullable = false, length = 100)
     private String gconfCode;
     
     @Column(name = "gconf_value", nullable = false, length = 255)
     private String gconfValue;
-    
-    @Column(name = "last_calibration_date")
-    private LocalDateTime lastCalibrationDate;
+
 }

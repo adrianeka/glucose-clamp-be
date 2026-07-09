1a8c09503ab539f780abb9c53937ec3df7d58d95
Adrian Eka <78060253+adrianeka@users.noreply.github.com>
2026-07-09 13:43:22 +0700

    fix: set timezone application

--- src/main/java/com/tujuhsembilan/glucoseclamp/GlucoseclampApplication.java
+++ src/main/java/com/tujuhsembilan/glucoseclamp/GlucoseclampApplication.java
@@ -4,10 +4,17 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import jakarta.annotation.PostConstruct;
+
 @SpringBootApplication(scanBasePackages = { "com.tujuhsembilan.glucoseclamp", "lib.i18n", "lib.minio"})
 @EnableJpaAuditing
 public class GlucoseclampApplication {
 
+	@PostConstruct
+	public void init() {
+		// Set Spring Boot SetTimeZone
+		java.util.TimeZone.setDefault(java.util.TimeZone.getTimeZone("Asia/Jakarta"));
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(GlucoseclampApplication.class, args);
 	}
